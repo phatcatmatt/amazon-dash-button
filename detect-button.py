117500d8817cb2d4638ace068425351cf68bbde5
Matt <phatcatmatt@gmail.com>
2017-08-05 21:35:21 -0600

    added blink module to dash button script. triggers LEDs to blink on button press

--- detect-button.py
+++ detect-button.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+from blinkrandom import blink
 
 MAC_ADDRESS = '68:37:e9:5a:e3:d0'
 
@@ -6,5 +7,6 @@ def arp_display(pkt):
     if pkt[ARP].op == 1:
         if pkt[ARP].hwsrc ==  MAC_ADDRESS:
             print ("dash button pressed!")
+            blink(24, 25)
 
 print (sniff(prn=arp_display, filter="arp", store=0, count=0))
