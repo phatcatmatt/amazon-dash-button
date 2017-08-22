f75911f1937a007f501741999fa5f1859416eab0
Matt <phatcatmatt@gmail.com>
2017-08-21 22:14:35 -0600

    added google maps module to script. now fetches travel time from api

--- detect-button.py
+++ detect-button.py
@@ -1,5 +1,7 @@
 from scapy.all import *
-from blinkrandom import blink
+from keys import * 
+from distance import travel_time
+import math
 
 MAC_ADDRESS = '68:37:e9:5a:e3:d0'
 
@@ -7,6 +9,7 @@ def arp_display(pkt):
     if pkt[ARP].op == 1:
         if pkt[ARP].hwsrc ==  MAC_ADDRESS:
             print ("dash button pressed!")
-            blink(24, 25)
+            time = travel_time()
+            print (math.ceil(time/60))
 
 print (sniff(prn=arp_display, filter="arp", store=0, count=0))
---
+++ distance.py
@@ -0,0 +1,8 @@
+import googlemaps
+from datetime import datetime
+from keys import maps_key
+
+def travel_time():
+  gmaps = googlemaps.Client(key=maps_key)
+  matrix_response = gmaps.distance_matrix("Provo, Utah", "Salt Lake City, Utah")
+  return matrix_response['rows'][0]['elements'][0]['duration']['value']
