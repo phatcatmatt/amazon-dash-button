1e4d7f30adabdce6690e557a57ce78f2c95af80b
Matt <phatcatmatt@gmail.com>
2017-08-22 21:54:45 -0600

    fixed indentation

--- distance.py
+++ distance.py
@@ -3,6 +3,6 @@ from datetime import datetime
 from keys import maps_key
 
 def travel_time():
-  gmaps = googlemaps.Client(key=maps_key)
-  matrix_response = gmaps.distance_matrix('Provo, Utah', 'Salt Lake City, Utah')
-  return matrix_response['rows'][0]['elements'][0]['duration']['value']
+    gmaps = googlemaps.Client(key=maps_key)
+    matrix_response = gmaps.distance_matrix('Provo, Utah', 'Salt Lake City, Utah')
+    return matrix_response['rows'][0]['elements'][0]['duration']['value']
